8ebfabe8325af5ccf2959ad47c728d586a0f29c8
Your Name <you@example.com>
2020-10-30 21:21:04 +0800

    Hello

--- HelloGit/src/main/java/com/atguigu/git/HelloGit.java
+++ HelloGit/src/main/java/com/atguigu/git/HelloGit.java
@@ -1,7 +1,12 @@
 package com.atguigu.git;
 
+
+
 public class HelloGit {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+
+    {
         System.out.println("Hello Git!");
     }
 }
+
