ff4ba51c97fa850d9309328d0851ec2d45d45da0
Your Name <you@example.com>
2020-11-01 12:29:49 +0800

    主干添加

--- HelloGit/src/main/java/com/atguigu/git/HelloGit.java
+++ HelloGit/src/main/java/com/atguigu/git/HelloGit.java
@@ -8,8 +8,9 @@ public class HelloGit {
     {
         System.out.println("Hello Git!");
         System.out.println("更新1!");
-        System.out.println("更新2!");
+        System.out.println("更新2        System.out.println(\"创建分支！\");!");
         System.out.println("创建分支！");
+        System.out.println("主干添加！");
     }
 }
 
